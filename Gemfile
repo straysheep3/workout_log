5e59849c1e9d8f23fbb50debfc6e22aab09d7d60
straysheep3 <getloose.kage@gmail.com>
2016-05-30 20:17:39 +0900

    Add gem

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'haml', '~> 4.0', '>= 4.0.7'
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,44 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :update, :edit, :destroy]
+
   def index
     #code
   end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def show
+  end
+
+  def edit
+    #code
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      redirect_to "new"
+    end
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+  private
+    def workout_params
+      params.require(:workout).permit(:date, :workout, :mood, :length)
+    end
+
+    def find_workout
+      @workout = Workout.find(params[:id])
+    end
 end
