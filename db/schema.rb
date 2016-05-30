dbb524b2f219b69739584d9a9eb7f31bbf8611a5
straysheep3 <getloose.kage@gmail.com>
2016-05-30 22:37:31 +0900

    exercisec model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530102804) do
+ActiveRecord::Schema.define(version: 20160530132403) do
+
+  create_table "execises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sets"
+    t.integer  "reps"
+    t.integer  "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "execises", ["workout_id"], name: "index_execises_on_workout_id"
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sets"
+    t.integer  "reps"
+    t.integer  "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "exercises", ["workout_id"], name: "index_exercises_on_workout_id"
 
   create_table "workouts", force: :cascade do |t|
     t.datetime "date"
